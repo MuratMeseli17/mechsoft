d950137048a3470458a1fd31a6da2346cf7b8554
MuratMeseli17 <35399543+MuratMeseli17@users.noreply.github.com>
2018-01-13 17:42:18 +0300

    Initial commit

---
+++ MuratProje/FormDAL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuratProje
+{
+    public class FormDAL
+    {
+        kayitEntities db = new kayitEntities();
+
+        public IEnumerable<Form> GetAllForm()
+        {
+            return db.Form;
+        }
+        public Form GetFormById(int id)
+        {
+            return db.Form.Find(id);
+        }
+        public Form CrateForm(Form form)
+        {
+            db.Form.Add(form);
+            db.SaveChanges();
+            return form;
+        }
+
+        public Form CrateForm(object form)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Form UpdateForm (int id,Form form)
+        {
+            db.Entry(form).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return form;
+        }
+        public void DeleteForm(int id)
+        {
+            db.Form.Remove(db.Form.Find(id));
+            db.SaveChanges();
+        }
+
+    }
+}
